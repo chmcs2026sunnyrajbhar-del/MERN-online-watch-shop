9b5aa0ab972fa56213aa1dadb9a41e096af3d419
chmcs2026sunnyrajbhar-del <chmcs2026.sunny.rajbhar@gmail.com>
2026-03-09 11:39:03 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://mern-online-watch-shop-backend.onrender.com/"
+  baseURL: "https://mern-online-watch-shop-backend.onrender.com/api",
 });
 
 export default api;
